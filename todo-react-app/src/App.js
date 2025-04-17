c7443258b0208c1c40de05d70652c2f89bd3f8e8
yeonseo0612 <qnq11@naver.com>
2025-04-17 18:21:25 +0900

    250417 수업

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 //경로는 현재파일을
-import { Greeting,Farewell } from './Greeting';
 import Todo from './Todo';
-import { Exmaple } from './Example';
+import {List, Paper} from '@mui/material';
 
 function App() {
+  //하나의 할 일을 객체로 관리할 것이다.
+  //id, title, done  
+  const [items, setItem] = useState([
+    {
+      id: "0",
+      title: "Hello world 1",
+      done: true
+  },
+  {
+    id: "1",
+    title: "Hello world 2",
+    done: false
+  }]);
+
+  //react는 key속성에 들어있는 값을 참고해서, 리스트의 요소가 변경될 경우
+  //어떤 요소가 변경되었는지 빠르게 파악할 수 있다.
+  const todoItems = items.length > 0 && 
+    //Paper컴포넌트
+    //종이 같은 표면 효과를 제공하는 컨테이너 컴포넌트
+    //elevation(그림자 깊이)를 통해 높낮이를 표현하고
+    //배경색과 그림자 효과로 콘텐츠를 돋보이게 한다.
+    <Paper style={{margin: 16}}>
+      <List> {/*일련의 항목을 세로로 나열하는 컨테이너 역할*/}
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+          ))}
+      </List>
+    </Paper>
+
   return (
     <div className="App">
-      <Exmaple />
-      {/* <Todo />
-      <Todo />
-      <Todo /> */}
+      {todoItems}
+      {/* <Todo item={item2}/> */}
       {/*컴포넌트의 호출 <컴포넌트명 /> */}
-      {/* <Greeting name="John" />
-      <Farewell name="John"/> */}
     </div>
   );
 }
 
 export default App;
+
+//1. Todo를 하나 더 만들어 item을 하나 더 넘겨보자
+//id = '1' , title = "Hello world 2" , done = false
+
+//2. Todo를 두 개 연속으로 늘어 놓는 대신, 배열과 반복문을 이용해보자
+//반복문으로 생성된 Todo컴포넌트들을 어떻게 넘길것인가?
--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
+import {ListItem, ListItemText, InputBase, Checkbox} from '@mui/material';
 //현재 파일에서는 checkBox와 label 컴포넌트를 만들어보자
 // export function Todo(){}
-let Todo = () => {
+let Todo = (props) => {
+
+    const [item, setItem] = useState(props.item);
+
     return(
         //html코드가 들어가는 부분
         //속성을 쓸 때 카멜케이스로 작성한다.
         //onclick -> onClick
         //classname -> className
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
+        <ListItem>
+            <Checkbox checked={item.done}/>
             {/* label태그는 for속성에 name값으로 연결해서 어떤 요소와 연결될 지 지정 */}
-            <label for="todo0">Todo 컴포넌트 만들기</label>
-        </div>
+            <ListItemText>
+                <InputBase 
+                    inputProps={{"aria-label" : "naked"}}
+                    type = "text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true} />
+            </ListItemText>
+        </ListItem>
     )
 }
 
