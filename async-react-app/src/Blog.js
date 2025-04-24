6f2eb53853ebc09ae395125dd9b128ce440764b1
hms1218 <rhkwmq93@naver.com>
2025-04-24 16:40:20 +0900

    250424 수업

--- async-react-app/src/Blog.js
+++ async-react-app/src/Blog.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Blog = () => {
 
@@ -45,13 +46,32 @@ export const Blog = () => {
     //게시물 추가
     //jsonPlaceholder에 추가를 요청한다고 해서 진짜로 추가되는것은 아님
     //state에 새 게시물을 추가하기
-    const addPost = () => {
-
+    const addPost = async() => {
+        axios.post("https://jsonplaceholder.typicode.com/posts",newPost)
+            .then((response) => {
+                // console.log(response.data);
+                setPosts([response.data,...posts])
+                setNewPost({title:'', body:''});
+            })
+            .catch((error) => {
+                setError(error.message);
+            })
     }
 
     //게시글 삭제
-    //필터링해서 다시 렌더링하기
+    //내가 원하는 글만 삭제하고 나머지를 필터링해서 다시 렌더링하기
+    const deletePost = async (id) => {
+        axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        setPosts(posts.filter(post => post.id !== id))
+    }
 
+    // http 메서드
+    // -> 클라이언트가 서버에게 요청을 할 때 수행할 동작을 지정하는 것.
+    //    각 메서드는 요청에 목적이 있다.
+    // GET : 서버로부터 데이터를 조회한다.
+    // POST : 서버에 데이터를 보내 추가한다.
+    // PUT : 데이터를 수정한다.
+    // DELETE : 데이터를 삭제한다.
 
     if(loading){
         return <h2>로 딩 중 . . . . . . </h2>
@@ -67,7 +87,7 @@ export const Blog = () => {
             <h2>새 게시물 추가</h2>
             <input type='text' placeholder="제목" value={newPost.title} onChange={(e) => setNewPost({...newPost, title:e.target.value})}/>
             <textarea placeholder="내용" value={newPost.body} onChange={(e) => setNewPost({...newPost, body:e.target.value})}/>
-            <button onClick={() => {}}>추가</button>
+            <button onClick={addPost}>추가</button>
             {/* 게시물 리스트 */}
             <div>
                 <h2>게시물 리스트</h2>
@@ -75,7 +95,7 @@ export const Blog = () => {
                     <div key={post.id} style={{border:'1px solid black', margin: '10px', padding:'10px'}}>
                         <h3>{post.title}</h3>
                         <p>{post.body}</p>
-                        <button onClick={() => {}}>삭제</button>
+                        <button onClick={() => {deletePost(post.id)}}>삭제</button>
                     </div>
                 ))}
             </div>
